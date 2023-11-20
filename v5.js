0fbde16835ec2ccf290d478ec3aa91dbeb4a31e1
Tyler Broussard <115663638+oscarpark0@users.noreply.github.com>
2023-11-19 23:53:04 -0500

    Update v5.js

--- v5.js
+++ v5.js
@@ -1,34 +1,42 @@
-// Variables
 let deck_id;
 let playerChips = 50;
 let pot = 0;
 let aceValue = null;
+let handHistory = [];
+let card1, card2, card3;
 // Elements
 let resultElement;
 let drawButton;
-let betButton;
+let bet1Button, bet5Button;
 let newGameButton;
+let shootThePotButton;
+
 document.addEventListener('DOMContentLoaded', function() {
   resultElement = document.getElementById('result');
   drawButton = document.getElementById('draw-button');
-  betButton = document.getElementById('bet-button');
+  bet1Button = document.getElementById('bet-1-button');
+  bet5Button = document.getElementById('bet-5-button');
   newGameButton = document.getElementById('new-game-button');
+  shootThePotButton = document.getElementById('shoot-the-pot-button');
+
   // Event listeners for Ace high or low buttons
   Array.from({ length: 2 }, (_, i) => i * 2 + 1).forEach(cardNumber => {
     ['high', 'low'].forEach(type => {
-      const button = document.getElementById(`ace-${type}-button-${cardNumber}`);
+      const button = document.getElementById(`ace-${type}-button-1`);
       if (button) {
         button.addEventListener('click', function() {
           aceValue = (type === 'high') ? 14 : 1;
-          displayAceButtons(cardNumber, 'none');
+          displayAceButtons('1', 'none');
         });
       } else {
-        console.error(`Button with id ace-${type}-button-${cardNumber} not found`);
+        console.error(`Button with id ace-${type}-button-1 not found`);
       }
     });
   });
+
   // Shuffle deck
   shuffleDeck();
+
   // Event listener for rules button
   let rulesButton = document.getElementById('rules-button');
   let rules = `Rank of Cards
@@ -38,61 +46,86 @@ Object of the Game
 The goal is to be the player with the most chips at the end of the game.
 
 The Ante
-50 Chips are distributed to the player, and the player puts one chip in the center of the table to form a pool or pot.
+50 Chips are distributed to the player, and the player puts one chip in the center of the table to form a pool or pot.`;
 
-
-The Betting
-The player may bet up to the entire pot or any portion of the number of chips in the pot, but they must always bet a minimum of one chip. When the player has placed a bet, the dealer turns up the top card from the pack and places it between the two cards already face up. If the card ranks between the two cards already face up, the player wins and takes back the amount of his bet plus an equivalent amount from the pot. If the third card is not between the face-up cards, the player loses his bet, and it is added to the pot. If the card turned up is the same as either of the two face up cards, the player must re-pay their initial bet. If the two face-up cards are the same, the player wins two chips and, again, no third card is turned up. 
-
-"Acey-Deucey" (ace, 2) is the best combination, and a player tends to bet the whole pot, if they can. This is because the only way an ace-deuce combination can lose is if the third card turned up is also an ace or a deuce.
-
-After the first player has finished, the dealer clears away the cards and places them face down in a pile. The next player then places a bet, and the dealer repeats the same procedure until all the players, including the dealer, have had a turn.
-
-If at any time, the pot has no more chips in it (because a player has "bet the pot" and won), each player again puts in one chip to restore the pot.
-
-When every player has had a turn to bet, the deal passes to the player on the dealer's left, and the game continues.`;
   rulesButton.addEventListener('click', function() {
     alert(rules);
   });
+
   // Draw button event listener
   drawButton.addEventListener('click', function() {
     resultElement.textContent = '';
     dealInitialCards();
-    betButton.disabled = false;
     drawButton.disabled = true;
+    shootThePotButton.disabled = false;
+    bet1Button.disabled = false;
+    bet5Button.disabled = false;
   });
-  // Bet button event listener
-  betButton.addEventListener('click', function() {
-    let playerBet = getBet(pot);
-    pot += playerBet;
-    updateDisplay();
-    dealCards(1).then(([card2]) => {
-      displayCard(card2, 'card2');
-      let card1Element = document.getElementById('card1-image').firstElementChild;
-      let card3Element = document.getElementById('card3-image').firstElementChild;
-      let card1Value = card1Element ? getCardRank(card1Element.getAttribute('data-value'), '1') : null;
-      let card2Value = getCardRank(card2.value);
-      let card3Value = card3Element ? getCardRank(card3Element.getAttribute('data-value'), '3') : null;
-      // checking if card2Value equals to card1Value or card3Value
-      if (card2Value === card1Value || card2Value === card3Value) {
-        playerChips -= playerBet;  // decrease player's chips by the bet
-        pot += playerBet;  // add the bet to the pot
-        resultElement.textContent = 'Lose! Darn. Same Card. Pay Double.';
-      } else if (card2Value > Math.min(card1Value, card3Value) && card2Value < Math.max(card1Value, card3Value)) {
-        let winnings = 2 * playerBet;
-        winnings = (pot < winnings) ? pot : winnings;  // checking if pot < winnings and return pot instead
-        playerChips += winnings;  // adding back to player's chip
-        pot -= winnings;
-        resultElement.textContent = 'Win! Nice.';
-      } else {
-        resultElement.textContent = 'Lose! Darn.';
-      }
+
+
+
+  function processBet(betAmount, winMessage = 'Win! Nice.', loseMessage = 'Lose! Darn.') {
+    let playerBet = betAmount;
+    if (playerChips >= playerBet) {
+      playerChips -= playerBet;  // decrease player's chips by the bet
+      pot += playerBet;  // add the bet to the pot
       updateDisplay();
-      drawButton.disabled = (playerChips > 0) ? false : true;
-      newGameButton.disabled = !drawButton.disabled;
-    });
-    betButton.disabled = true;
+      dealCards(1).then(([card2]) => {
+        displayCard(card2, 'card2');
+        let card1Element = document.getElementById('card1-image').firstElementChild;
+        let card3Element = document.getElementById('card3-image').firstElementChild;
+        let card1Value = card1Element ? getCardRank(card1Element.getAttribute('data-value'), '1') : null;
+        let card2Value = getCardRank(card2.value);
+        let card3Value = card3Element ? getCardRank(card3Element.getAttribute('data-value'), '3') : null;
+        // checking if card2Value equals to card1Value or card3Value
+        if (card2Value === card1Value || card2Value === card3Value) {
+          playerChips -= playerBet;  // decrease player's chips by the bet
+          pot += playerBet;  // add the bet to the pot
+          resultElement.textContent = loseMessage + ' Same Card. Pay Double.';
+        } else if (card2Value > Math.min(card1Value, card3Value) && card2Value < Math.max(card1Value, card3Value)) {
+          let winnings = 2 * playerBet;
+          winnings = (pot < winnings) ? pot : winnings;  // checking if pot < winnings and return pot instead
+          playerChips += winnings;  // adding back to player's chip
+          pot -= winnings;
+          resultElement.textContent = winMessage;
+        } else {
+          resultElement.textContent = loseMessage;
+        }
+        updateDisplay();
+        displayHandHistory();
+        drawButton.disabled = (playerChips > 0) ? false : true;
+        newGameButton.disabled = !drawButton.disabled;
+        bet1Button.disabled = true;
+        bet5Button.disabled = true;
+        shootThePotButton.disabled = true;
+          handHistory.push({
+            result: resultElement.textContent,
+            chips: playerChips,
+            pot: pot,
+            betAmount: playerBet,
+            card1: card1,
+            card2: card2,
+            card3: card3
+          });
+          displayHandHistory();
+      });
+    } else {
+      console.error('Not enough chips to bet');
+    }
+  }
+
+  bet1Button.addEventListener('click', function() {
+    processBet(1);
   });
+
+  bet5Button.addEventListener('click', function() {
+    processBet(5);
+  });
+
+  shootThePotButton.addEventListener('click', function() {
+    processBet(pot, 'Wow! Nice Shot!', 'Shoot!');
+  });
+
   // New game button event listener
   newGameButton.addEventListener('click', function() {
     resultElement.textContent = '';
@@ -101,94 +134,192 @@ When every player has had a turn to bet, the deal passes to the player on the de
     updateDisplay();
     shuffleDeck();
     newGameButton.disabled = true;
+    shootThePotButton.disabled = true;
+    bet1Button.disabled = true;
+    bet5Button.disabled = true;
   });
+
   // Listen for page loaded event
   document.addEventListener('DOMContentLoaded', (event) => {
     resultElement.textContent = '';
     shuffleDeck();
   });
 });
+
 // Shuffle deck function
 async function shuffleDeck() {
   try {
     let response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
     let data = await response.json();
     deck_id = data.deck_id;
-    document.getElementById('bet-button').disabled = false;
     await dealInitialCards();
   } catch (error) {
     console.error('Shuffle deck failed', error);
   }
 }
+
 // Deal cards
 async function dealCards(count) {
+  //console.log(`Dealing ${count} cards`);
   try {
     let response = await fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${count}`);
     let data = await response.json();
+    //console.log(`Fetched cards: ${JSON.stringify(data.cards)}`);
     // check if there are fewer than 3 cards remaining
     if (data.remaining <= 3) {
       shuffleDeck();  // shuffle the deck if there are not enough cards
     }
     return data.cards;
+    ;
   } catch (error) {
     console.error('Deal cards failed', error);
   }
+  displayHandHistory();
 }
 // Deal the first and third cards
 async function dealInitialCards() {
   try {
-    let [card1, card3] = await dealCards(2);
+    [card1] = await dealCards(1);
     displayCard(card1, 'card1');
-    displayCard(card3, 'card3');
     displayBackOfCard('card2');
-    if (card1.value === card3.value) {
-      playerChips += 2;
-      updateDisplay();
-      resultElement.textContent = 'Automatic Win! You got a pair. +2 chips.';
-      betButton.disabled = true;
-      setTimeout(function() {
-        drawButton.disabled = false;
-        resultElement.textContent = ''; // Clear the result message
-        shuffleDeck();
-      }, 2000);  // 2000 milliseconds = 2 seconds
-      return;
-    }
+
     if (card1.value === 'ACE') {
       displayAceButtons('1', 'block');
-    } else if (card3.value === 'ACE') {
-      displayAceButtons('3', 'block');
+      shootThePotButton.disabled = true;
+      displayBackOfCard('card3'); // Display the back of card in card 3 position
+
+      // Add event listener to Ace high and low buttons
+      ['high', 'low'].forEach(type => {
+        const button = document.getElementById(`ace-${type}-button-1`);
+        if (button) {
+          button.addEventListener('click', function() {
+            aceValue = (type === 'high') ? 14 : 1;
+            displayAceButtons('1', 'none');
+            dealCards(1).then(([card3]) => {
+              displayCard(card3, 'card3');
+              bet1Button.disabled = false;
+              bet5Button.disabled = false;
+              drawButton.disabled = true;
+            });
+          });
+        } else {
+          console.error(`Button with id ace-${type}-button-1 not found`);
+        }
+      });
+    } else {
+      [card3] = await dealCards(1);
+      displayCard(card3, 'card3');
+      if (card1.value === card3.value && playerChips >= 4) {
+       // let split = confirm("You have drawn two identical cards. Do you want to split and play 2 new hands? This will cost you 4 chips.");
+        if (split) {
+          playerChips -= 4; // deduct 4 chips from the player's chips
+          updateDisplay();
+          let [newCard1, newCard2] = await dealCards(2); // draw 2 new cards
+          displayCard(newCard1, 'card2a'); // display the new cards
+          displayCard(newCard2, 'card2b');
+          document.getElementById('split-cards').style.display = 'block'; // show the split cards
+        } else {
+          document.getElementById('split-cards').style.display = 'none';
+          document.getElementById('card2').style.display = 'block'; // hide the split cards
+        }
+      } else if (card1.value === card3.value) {
+        playerChips += 2;
+        updateDisplay();
+        resultElement.textContent = 'Automatic Win! You got a pair. +2 chips.';
+        bet1Button.disabled = true;
+        bet5Button.disabled = true;
+        setTimeout(function() {
+          drawButton.disabled = false;
+          resultElement.textContent = ''; // Clear the result message
+          shuffleDeck();
+        }, 2000);  // 2000 milliseconds = 2 seconds
+      } else {
+        // Hide the split cards and show card2
+        document.getElementById('split-cards').style.display = 'none';
+        document.getElementById('card2').style.display = 'block';
+
+        // Enable the buttons if no pair is drawn and card 1 is not an Ace
+        bet1Button.disabled = false;
+        bet5Button.disabled = false;
+        drawButton.disabled = true;
+      }
     }
   } catch (error) {
     console.error('Deal initial cards failed', error);
   }
 }
-// Get player bet
-function getBet(potSize) {
-  let bet = document.getElementById('bet-input').value;
-  // validation
-  bet = Math.max(1, Math.min(bet, potSize, playerChips));
-  playerChips -= bet;
-  return bet;
+
+function displayHandHistory() {
+  const handHistoryElement = document.getElementById('hand-history');
+  handHistoryElement.innerHTML = ''; // Clear the previous hand history
+
+  // Create table
+  const table = document.createElement('table');
+
+  // Create table header
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+  ['Hand', 'Result', 'Chips', 'Bet', 'Pot', 'Card 1', 'Card 2', 'Card 3'].forEach(header => {
+    const th = document.createElement('th');
+    th.textContent = header;
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // Create table body
+  const tbody = document.createElement('tbody');
+  handHistory.forEach((hand, index) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${hand.result}</td>
+      <td>${hand.chips}</td>
+      <td>${hand.betAmount}</td>
+      <td>${hand.pot}</td>
+      <td><img class="hand-history-card" src="${hand.card1.image}" data-value="${hand.card1.value}"></td>
+      <td><img class="hand-history-card" src="${hand.card2.image}" data-value="${hand.card2.value}"></td>
+      <td><img class="hand-history-card" src="${hand.card3.image}" data-value="${hand.card3.value}"></td>
+    `;
+    tbody.appendChild(row);
+  });
+  table.appendChild(tbody);
+
+  handHistoryElement.appendChild(table);
+
+  const handHeight = handHistoryElement.firstChild.offsetHeight;
+
+  handHistoryElement.style.height = `${handHeight * 4}px`;
 }
+
 // Update chips and pot display
 function updateDisplay() {
+ // console.log(`Updating display: playerChips=${playerChips}, pot=${pot}`);
   document.getElementById('chips-count').textContent = playerChips;
   document.getElementById('pot-count').textContent = pot;
+  bet1Button.disabled = (playerChips < 1);
+  bet5Button.disabled = (playerChips < 5);
+  shootThePotButton.disabled = (pot === 0);
 }
+
+
 // Display card
 function displayCard(card, elementId) {
+  const cardElement = document.getElementById(`${elementId}-image`);
+  while (cardElement.firstChild) {
+    cardElement.removeChild(cardElement.firstChild);
+  }
   if (card && card.image) {
-    let cardElement = document.getElementById(`${elementId}-image`);
-    cardElement.innerHTML = '';
-    let img = document.createElement('img');
+    const img = document.createElement('img');
     img.src = card.image;
-    // Change 'data-value' attribute to the actual card value
     img.setAttribute('data-value', card.value);
     cardElement.appendChild(img);
   } else {
     console.error('Invalid card data');
   }
 }
+
+// Display back of card
 function displayBackOfCard(elementId) {
   let cardElement = document.getElementById(`${elementId}-image`);
   cardElement.innerHTML = '';
@@ -196,6 +327,7 @@ function displayBackOfCard(elementId) {
   img.src = 'https://deckofcardsapi.com/static/img/back.png';
   cardElement.appendChild(img);
 }
+
 // Display Ace high or low buttons
 function displayAceButtons(cardNumber, displayStyle) {
   ['high', 'low'].forEach(type => {
@@ -208,10 +340,23 @@ function displayAceButtons(cardNumber, displayStyle) {
     }
   });
 }
+
+function bet(amount) {
+  //console.log(`Betting ${amount} chips`);
+  if (playerChips >= amount) {
+    playerChips -= amount;
+    pot += amount;
+    //console.log(`Player now has ${playerChips} chips and pot is ${pot}`);
+    updateDisplay();
+  } else {
+    console.error('Not enough chips to bet');
+  }
+}
+
 // Get card rank
-function getCardRank(value) {
+function getCardRank(value, position) {
   if (value === 'ACE') {
-    return aceValue; // use the value stored in aceValue for Ace card
+    return position === '3' ? 14 : aceValue; // use 14 for Ace card in position 3
   } else if (value === 'JACK') {
     return 11;
   } else if (value === 'QUEEN') {
